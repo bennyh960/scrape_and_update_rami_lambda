e95ce8897b24369770400a8cbf0651adb827b26e
bennyh960 <bennyh960@gmail.com>
2023-04-28 17:58:39 +0300

    change dns to ip in lambda

--- draft.js
+++ draft.js
@@ -1,27 +1,44 @@
 import https from "https";
 
-const options = {
-  hostname: "blog.jakoblind.no",
-  path: "/aws-lambda-github-actions/",
-  method: "GET",
+export const handler = (event, context, callback) => {
+  const headers = {
+    Accept:
+      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+    Pragma: "no-cache",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+    "sec-ch-ua": '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+  };
+  const params = {
+    host: "publishedprices.co.il",
+    path: "/login",
+    headers,
+    // rejectUnauthorized: false
+  };
+  const req = https.request(params, function (res) {
+    let data = "";
+    console.log("STATUS: " + res.statusCode);
+    res.setEncoding("utf8");
+    res.on("data", function (chunk) {
+      data += chunk;
+    });
+    res.on("end", function () {
+      console.log("DONE");
+      console.log(data);
+    });
+  });
+  req.end();
 };
 
-const req = https.request(options, (res) => {
-  console.log(`statusCode: ${res.statusCode}`);
-
-  let data = "";
-
-  res.on("data", (chunk) => {
-    data += chunk;
-  });
-
-  res.on("end", () => {
-    console.log(data.slice(1, 500));
-  });
-});
-
-req.on("error", (error) => {
-  console.error(error);
-});
-
-req.end();
+handler();
